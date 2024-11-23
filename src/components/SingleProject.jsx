d88fe2c6f5d50d6cd89dd2d0402a425ff00c13b4
Meez <meez@Meezs-MBP.fios-router.home>
2024-11-23 17:43:20 -0600

    working on project page

---
+++ src/components/SingleProject.jsx
@@ -0,0 +1,9 @@
+function singleProject() {
+    return <section>
+        <h3>Project Name</h3>
+        <img src = ""></img>
+        <a href= "">Github URL : ""</a>
+    </section>
+};
+
+export default singleProject;
---
+++ src/pages/portfolio.jsx
@@ -0,0 +1,10 @@
+import singleProject from "../components/SingleProject";
+
+// change imgUrl to save img of Employee-Tracker
+const projects = [{name: "Employee-Tracker", imgUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcReiKeTsm26jLOx1RQhXGkRSPWNj2tCeMKdUA&s", githubUrl: "https://github.com/sameeridris/Employee-Tracker"}];
+
+function Portfolio() {
+    return <section>
+        <h3></h3>
+    </section>
+}
