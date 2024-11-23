bd2f2cbf1d1d19684f9a8b1195e1438a7bdeb3b4
Meez <meez@Meezs-MBP.fios-router.home>
2024-11-23 17:56:39 -0600

    almost finished with portfolio page

--- src/components/SingleProject.jsx
+++ src/components/SingleProject.jsx
@@ -1,9 +1,9 @@
-function singleProject() {
+function SingleProject(props) {
     return <section>
-        <h3>Project Name</h3>
-        <img src = ""></img>
-        <a href= "">Github URL : ""</a>
+        <h3>{props.name}</h3>
+        <img src = {props.imgUrl}></img>
+        <a href= {props.githubUrl}>Github URL : ""</a>
     </section>
 };
 
-export default singleProject;
+export default SingleProject;
--- src/pages/portfolio.jsx
+++ src/pages/portfolio.jsx
@@ -1,10 +1,12 @@
-import singleProject from "../components/SingleProject";
+import SingleProject from "../components/SingleProject";
 
-// change imgUrl to save img of Employee-Tracker
-const projects = [{name: "Employee-Tracker", imgUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcReiKeTsm26jLOx1RQhXGkRSPWNj2tCeMKdUA&s", githubUrl: "https://github.com/sameeridris/Employee-Tracker"}];
+// change imgUrl to save img of all projects
+const projects = [{name: "Employee-Tracker", imgUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcReiKeTsm26jLOx1RQhXGkRSPWNj2tCeMKdUA&s", githubUrl: "https://github.com/sameeridris/Employee-Tracker"}, {name: "README-generator", imgUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcReiKeTsm26jLOx1RQhXGkRSPWNj2tCeMKdUA&s", githubUrl: "https://github.com/sameeridris/README-generator"}, {name: "Weather-Dashboard", imgUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcReiKeTsm26jLOx1RQhXGkRSPWNj2tCeMKdUA&s", githubUrl: "https://github.com/sameeridris/Weather-Dashboard"}, {name: "README-generator", imgUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcReiKeTsm26jLOx1RQhXGkRSPWNj2tCeMKdUA&s", githubUrl: "https://github.com/sameeridris/README-generator"}, ];
 
 function Portfolio() {
     return <section>
-        <h3></h3>
+        {projects.map(p => <SingleProject name = {p.name} imgUrl = {p.imgUrl} githubUrl= {p.githubUrl}></SingleProject>)}
     </section>
-}
+};
+
+export default Portfolio;
