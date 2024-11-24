b64fc82ac70a3d2403d07c6db37a1dcd50e0a904
Meez <meez@Meezs-MBP.fios-router.home>
2024-11-23 18:06:05 -0600

    updated the portfolio page

--- src/pages/portfolio.jsx
+++ src/pages/portfolio.jsx
@@ -1,7 +1,7 @@
 import SingleProject from "../components/SingleProject";
 
 // change imgUrl to save img of all projects
-const projects = [{name: "Employee-Tracker", imgUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcReiKeTsm26jLOx1RQhXGkRSPWNj2tCeMKdUA&s", githubUrl: "https://github.com/sameeridris/Employee-Tracker"}, {name: "README-generator", imgUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcReiKeTsm26jLOx1RQhXGkRSPWNj2tCeMKdUA&s", githubUrl: "https://github.com/sameeridris/README-generator"}, {name: "Weather-Dashboard", imgUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcReiKeTsm26jLOx1RQhXGkRSPWNj2tCeMKdUA&s", githubUrl: "https://github.com/sameeridris/Weather-Dashboard"}, {name: "README-generator", imgUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcReiKeTsm26jLOx1RQhXGkRSPWNj2tCeMKdUA&s", githubUrl: "https://github.com/sameeridris/README-generator"}, ];
+const projects = [{name: "Employee-Tracker", imgUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcReiKeTsm26jLOx1RQhXGkRSPWNj2tCeMKdUA&s", githubUrl: "https://github.com/sameeridris/Employee-Tracker"}, {name: "README-generator", imgUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcReiKeTsm26jLOx1RQhXGkRSPWNj2tCeMKdUA&s", githubUrl: "https://github.com/sameeridris/README-generator"}, {name: "Weather-Dashboard", imgUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcReiKeTsm26jLOx1RQhXGkRSPWNj2tCeMKdUA&s", githubUrl: "https://github.com/sameeridris/Weather-Dashboard"}, {name: "Vehicle-Builder", imgUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcReiKeTsm26jLOx1RQhXGkRSPWNj2tCeMKdUA&s", githubUrl: "https://github.com/sameeridris/Vehicle-Builder"}, ];
 
 function Portfolio() {
     return <section>
